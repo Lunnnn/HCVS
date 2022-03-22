ccfdb1df068dbcfb2d7b48d6debdc4bd50f2aedb
Lunnnn <100186640+Lunnnn@users.noreply.github.com>
2022-03-22 14:20:59 +0800

    Update repeater.c

--- repeater.c
+++ repeater.c
@@ -5,8 +5,7 @@ int main() {
     int input;
 
     printf("每輸入一個數字，程式會再印出相同的數字。\n");
-    while(1) {
-        scanf("%d", &input);
+    while(scanf("%d", &input)){ 
         printf("%d\n", input);
     }
 }
